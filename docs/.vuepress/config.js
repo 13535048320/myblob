948e97b7631d1476555a4ea9d943aa0caf65008f
zhicheng.zhang@artisantechnologies.cn <zhicheng.zhang@artisantechnologies.cn>
2020-08-06 17:59:12 +0800

    first commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   title: "Zhang's blog",
   description: '运维技术博客,学习与总结。Nginx,Tomcat,Zabbix,Docker,Kubernetes,MySQL,MQ,GitLab等技术文章。', // 描述,以 <meta> 标签渲染到页面html中
   // base: '/vuepress-theme-vdoing/', // '/<github仓库名>/'， 默认'/'
-  base: 'myblob',
+  base: '/myblob/',
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown'}],
@@ -63,7 +63,7 @@ module.exports = {
       link: 'https://github.com/13535048320' // 可选的
     },
     blogger:{ // 博主信息，显示在首页侧边栏
-      avatar: '/img/20200805113001.jpg',
+      avatar: '/myblob/img/20200805113001.jpg',
       name: 'Zhang zc',
       slogan: '运维搬砖师'
     },
