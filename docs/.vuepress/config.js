807136afdffbcefa14d95b6dffe82c19639df0d8
zhicheng.zhang@artisantechnologies.cn <zhicheng.zhang@artisantechnologies.cn>
2020-08-06 17:09:49 +0800

    first commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -89,7 +89,7 @@ module.exports = {
     },
     footer:{ // 页脚信息
       createYear: 2020, // 博客创建年份
-      copyrightInfo: 'zhangzc | <a href="https://github.com/13535048320/myblob/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+      copyrightInfo: 'zhangzc |', // 博客版权信息，支持a标签
     }
   },
   plugins: [ // 插件
