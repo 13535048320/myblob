995dcf60547a7f82d7e31f10e4e172e32f219ae7
zhicheng.zhang@artisantechnologies.cn <zhicheng.zhang@artisantechnologies.cn>
2020-08-07 11:57:33 +0800

    first commit

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -40,10 +40,10 @@ module.exports = [
     text: '大数据',
     link: '/bigdata/',
     items: [
-      {text: '学习', link: '/pages/f2a556/'},
-      {text: '面试', link: '/pages/aea6571b7a8bae86/'},
-      {text: '心情杂货', link: '/pages/2d615df9a36a98ed/'},
-      {text: '友情链接', link: '/friends/'},
+      {text: 'zookeeper', link: '/pages/7ebd15/'},
+      {text: 'hadoop', link: '/pages/7a645a/'},
+      {text: 'hbase', link: '/pages/707eb4/'},
+      {text: '整合', link: '/pages/950f4c/'},
     ]
   },
   {
